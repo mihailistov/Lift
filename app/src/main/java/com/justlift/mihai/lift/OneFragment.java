026919f00ab17e5c14d8b9d53aafbbb4f6574660
mihai93 <mihailistov@gmail.com>
2016-03-26 17:48:09 -0400

    Tabs working with lists! WOOO

--- app/src/main/java/com/justlift/mihai/lift/OneFragment.java
+++ app/src/main/java/com/justlift/mihai/lift/OneFragment.java
@@ -1,15 +1,17 @@
 package com.justlift.mihai.lift;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 /**
  * Created by mihai on 16-03-26.
  */
-public class OneFragment extends Fragment {
+public class OneFragment extends ListFragment {
+    String[] list_items;
 
     public OneFragment() {
         // Required empty public constructor
@@ -23,6 +25,8 @@ public class OneFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        list_items = getResources().getStringArray(R.array.list);
+        setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list_items));
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_one, container, false);
     }
