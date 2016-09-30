8ba97faa41bba62584d5fe9214058a0ecb1d3b14
mihai93 <mihailistov@gmail.com>
2016-09-29 22:33:02 -0400

    List kinda working

--- app/src/main/java/ca/mihailistov/lift/AddExerciseActivity.java
+++ app/src/main/java/ca/mihailistov/lift/AddExerciseActivity.java
@@ -6,13 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -21,9 +20,11 @@ import ca.mihailistov.lift.Realm.RealmExerciseData;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
-public class AddExerciseActivity extends AppCompatActivity {
+public class AddExerciseActivity extends AppCompatActivity implements  RecyclerViewClickListener {
     private static final String TAG = "AddExerciseActivity";
     private Toolbar toolbar;
+    private TextView toolbarText;
+    private ImageView toolbarIcon;
     private RecyclerView mRecyclerView;
     private ExerciseAdapter exerciseAdapter;
     private Realm realm;
@@ -47,7 +48,11 @@ public class AddExerciseActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.dialog_toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Choose category");
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        toolbarText = (TextView) findViewById(R.id.add_exercise_title);
+        toolbarText.setText("Choose category");
+        toolbarIcon = (ImageView) findViewById(R.id.add_exercise_icon);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.add_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -63,39 +68,36 @@ public class AddExerciseActivity extends AppCompatActivity {
 //            categoryList.add(allRealmCategories.get(i).name);
 //        }
 
+        exerciseAdapter = new ExerciseAdapter(this, this);
         mRecyclerView.addItemDecoration(new SimpleDividerItemDecoration(getApplicationContext()));
-        exerciseAdapter = new ExerciseAdapter();
         mRecyclerView.setAdapter(exerciseAdapter);
 
     }
 
+    @Override
+    public void recyclerViewListClicked(View v, int position) {
+        TextView textView = (TextView) v.findViewById(R.id.category_text);
+        if (DEPTH == 0) {
+            String category = textView.getText().toString();
 
-    private class ExerciseHolder extends RecyclerView.ViewHolder {
-        private final TextView textView;
-        private String category;
-        private Context context;
-
-        public ExerciseHolder(View itemView) {
-            super(itemView);
-
-            textView = (TextView) itemView.findViewById(R.id.category_text);
-        }
+            toolbarText.setText(category);
 
 
-
-        public void bindCategoryList (String category) {
-            this.category = category;
-            textView.setText(category);
         }
     }
 
+    public class ExerciseAdapter extends RecyclerView.Adapter<ExerciseAdapter.ExerciseHolder> {
+        private Context context;
+        private RecyclerViewClickListener itemListener;
 
-    private class ExerciseAdapter extends RecyclerView.Adapter<ExerciseHolder> {
-        private View view;
+        public ExerciseAdapter(Context context, RecyclerViewClickListener itemListener){
+            this.context = context;
+            this.itemListener = itemListener;
+        }
 
         @Override
         public ExerciseHolder onCreateViewHolder(ViewGroup parent, int position) {
-            view = LayoutInflater.from(parent.getContext())
+            View view = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.list_item_add_exercise, parent, false);
 
             return new ExerciseHolder(view);
@@ -113,35 +115,54 @@ public class AddExerciseActivity extends AppCompatActivity {
                 exercise = exercisesInCat.get(position).name.toString();
                 holder.bindCategoryList(exercise);
             }
-            final String finalCategory = category;
-            final String finalExercise = exercise;
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (DEPTH == 0) {
-                        Toast.makeText(getApplication(), "Clicked on: " + finalCategory,
-                                Toast.LENGTH_SHORT).show();
-
-                        exercisesInCat = realm.where(RealmExerciseData.class)
-                                .equalTo("category", finalCategory).findAllSorted("name");
-                        Log.e(TAG, "Exercises found in category: " + exercisesInCat.size());
-                        DEPTH = 1; // Set DEPTH = 1 to incidate displaying EXERCISES
-                        exerciseAdapter.notifyDataSetChanged();
-                        mRecyclerView.invalidate();
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Clicked on: " + finalExercise,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
+//            final String finalCategory = category;
+//            final String finalExercise = exercise;
+//            view.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    if (DEPTH == 0) {
+//                        Toast.makeText(getApplication(), "Clicked on: " + finalCategory,
+//                                Toast.LENGTH_SHORT).show();
+//
+//                        exercisesInCat = realm.where(RealmExerciseData.class)
+//                                .equalTo("category", finalCategory).findAllSorted("name");
+//                        Log.e(TAG, "Exercises found in category: " + exercisesInCat.size());
+//                        DEPTH = 1; // Set DEPTH = 1 to incidate displaying EXERCISES
+//                        exerciseAdapter.notifyDataSetChanged();
+//                        mRecyclerView.invalidate();
+//                    } else {
+//                        Toast.makeText(getApplicationContext(), "Clicked on: " + finalExercise,
+//                                Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            });
         }
 
         @Override
         public int getItemCount() {
-            if (DEPTH == 1)
-                return exercisesInCat.size();
-            else
+            if (DEPTH == 0)
                 return allRealmCategories.size();
+            else
+                return exercisesInCat.size();
+        }
+
+        public class ExerciseHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+            private final TextView textView;
+
+            public ExerciseHolder(View itemView) {
+                super(itemView);
+                textView = (TextView) itemView.findViewById(R.id.category_text);
+                itemView.setOnClickListener(this);
+            }
+
+            public void bindCategoryList (String category) {
+                textView.setText(category);
+            }
+
+            @Override
+            public void onClick(View v) {
+                itemListener.recyclerViewListClicked(v, this.getLayoutPosition());
+            }
         }
     }
 }
---
+++ app/src/main/java/ca/mihailistov/lift/RecyclerViewClickListener.java
@@ -0,0 +1,12 @@
+package ca.mihailistov.lift;
+
+import android.view.View;
+
+/**
+ * Created by mihai on 2016-09-29.
+ */
+
+public interface RecyclerViewClickListener
+{
+    public void recyclerViewListClicked(View v, int position);
+}
